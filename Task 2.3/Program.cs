2bff39c09d9dbfffbdc97288aab0a751a691bcef
iDobryDen <59179395+iDobryDen@users.noreply.github.com>
2024-07-22 20:07:49 +0400

    task 2.3 done

--- Task 2.3/Program.cs
+++ Task 2.3/Program.cs
@@ -1,10 +1,17 @@
 ﻿namespace Task_2._3;
 
+//Напишите программу, которая считывает из консоли ваше имя и выводит
+//приветственное сообщение.
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Введите Ваше имя");
+
+        string Name = Console.ReadLine();
+
+        Console.WriteLine("Приветствую " + Name);
     }
 }
 
