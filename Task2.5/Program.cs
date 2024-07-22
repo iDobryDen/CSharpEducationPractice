d00804acd0c4fc2c26623276e20ea68dacdfda3d
iDobryDen <59179395+iDobryDen@users.noreply.github.com>
2024-07-22 20:35:00 +0400

    task 2.5 done

--- Task2.5/Program.cs
+++ Task2.5/Program.cs
@@ -1,10 +1,17 @@
 ﻿namespace Task2._5;
 
+//Напишите программу, которая вычисляет формулу площади круга и выводит
+//результат на экран консоли с понятным сообщением.Число π запишите как константу и
+//используйте её в формуле.Радиус запишите как переменную.Считывать из консоли ничего не нужно.
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        double pi = 3.14;
+        int R = 5;
+        double area_cirle = pi * R * R;
+        Console.WriteLine("area of ​​cirle:" + area_cirle);
     }
 }
 
