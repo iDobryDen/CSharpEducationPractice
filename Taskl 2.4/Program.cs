5754832f39a0ca23ba21b50a21d6e7a63bb07315
iDobryDen <59179395+iDobryDen@users.noreply.github.com>
2024-07-22 20:26:41 +0400

    task 2.4 done

--- Taskl 2.4/Program.cs
+++ Taskl 2.4/Program.cs
@@ -1,10 +1,19 @@
 ﻿namespace Taskl_2._4;
 
+//Напишите программу, которая считывает из консоли фразу и сохраняет её.
+//Затем считывает вторую фразу и выводит на экран консоли обе этих фразы,
+//объединённые в одно предложение. (Пример: ввели “Привет”, а затем “мир”. В
+//консоли отобразится “Привет мир”).
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Введите фразу: ");
+        string phrase1 = Console.ReadLine();
+        Console.WriteLine("Введите вторую фразу: ");
+        string phrase2 = Console.ReadLine();
+        Console.WriteLine(phrase1 +" " + phrase2);
     }
 }
 
