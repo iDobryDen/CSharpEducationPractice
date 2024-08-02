d6a77c6e198830e909af41b8850ee4708549f667
iDobryDen <59179395+iDobryDen@users.noreply.github.com>
2024-08-02 06:44:47 +0400

    Practice2.Task2.A done

--- Practice2.Task1/Practice2.Task2/Program.cs
+++ Practice2.Task1/Practice2.Task2/Program.cs
@@ -1,10 +1,23 @@
 ﻿namespace Practice2.Task2;
 
+//Напишите программу, которая переводит:
+    //a.метры в километры
+    //b.километры в сантиметры
+    //c.м/с в км/ч
+    //d. градусы C в градусы F
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine(" ");
+        Console.WriteLine("A. метры в километры.");
+
+        Console.WriteLine("Введите расстояние в метрах");
+        int metrs = int.Parse(Console.ReadLine());
+        float km = metrs / 1000f; // Используем 1000f для получения float значения
+
+        Console.WriteLine($"Расстояние в километрах равно: " + km);
     }
 }
 
