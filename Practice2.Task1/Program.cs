fb9bcc075e7ba7a8892eeffc1ef838df1d7a426e
iDobryDen <59179395+iDobryDen@users.noreply.github.com>
2024-08-02 05:52:04 +0400

    Practice2.Task1.E done

--- Practice2.Task1/Program.cs
+++ Practice2.Task1/Program.cs
@@ -45,9 +45,7 @@ class Program
 
         string[] phrase = new string[3];
 
-        int phraseLenght = 3;
-
-        for (int k = 0; k < phraseLenght; k++)
+        for (int k = 0; k < phrase.Length; k++)
         {
             Console.WriteLine("Введите слово из фразы");
             phrase[k] = Console.ReadLine();
@@ -56,6 +54,26 @@ class Program
         for (int m = 0; m < phrase.Length; m++)
         {
             Console.Write(phrase[m] + " ");
+        }
+
+        Console.WriteLine(" ");
+        Console.WriteLine("E. цикл while, в котором у пользователя запрашиваются слова, затем выводится полная фраза, " +
+                            " слова в которой разделены пробелами.");
+
+        string[] phrase2 = new string[3];
+        int count = 0;
+        while (count < phrase2.Length)
+        {
+            Console.WriteLine("Введите слово из фразы");
+            phrase2[count] = Console.ReadLine();
+            count++;
+        }
+
+        count = 0;
+        while (count < phrase2.Length)
+        {
+            Console.Write(phrase2[count] + " ");
+            count++;
         }
     }
 }
