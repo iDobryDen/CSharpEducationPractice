9cdf8e893011dd5433e1da509fb47d1b1bc8fba8
iDobryDen <59179395+iDobryDen@users.noreply.github.com>
2024-08-03 09:27:56 +0400

    Practice2.Task3 done

--- Practice2.Task1/Practice2.Task3/Program.cs
+++ Practice2.Task1/Practice2.Task3/Program.cs
@@ -1,10 +1,28 @@
 ﻿namespace Practice2.Task3;
 
+//Напишитепрограммудлявычислениявыражения(а + b — f / а) + f * a * a — (a + b). Все переменные задаются пользователем.Учтите деление на 0.
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Input a");
+        int a = int.Parse(Console.ReadLine());
+
+        if (a == 0)
+        {
+            Console.WriteLine("Переменная a не может быть равна 0");
+            return;
+        }
+
+        Console.WriteLine("Input b");
+        int b = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Input f");
+        int f = int.Parse(Console.ReadLine());
+
+        int result = (a + b - f / a) + f * a * a - (a + b);
+        Console.WriteLine($"выражение равно: " + result);
     }
 }
 
