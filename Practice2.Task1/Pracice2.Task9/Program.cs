a6563a575aad07dead2c32de792236bd99c466d1
iDobryDen <59179395+iDobryDen@users.noreply.github.com>
2024-08-03 11:09:13 +0400

    Practice2.Task9 done

--- Practice2.Task1/Pracice2.Task9/Program.cs
+++ Practice2.Task1/Pracice2.Task9/Program.cs
@@ -1,10 +1,29 @@
 ﻿namespace Pracice2.Task9;
 
+//Напишите метод, который создаёт массив целых чисел и возвращает его.
+//Размер массива нужно передавать в качестве аргумента.Вдобавок напишите
+//метод, который выводит переданный массив на экран консоли.
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int size = 3;
+        int[] array = Value(size);
+        for (int j = 0; j < array.Length; j++)
+        {
+            Console.WriteLine(array[j]);
+        }
+    }
+
+    static int[] Value(int size)
+    {
+        int[] array = new int[size];
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = i;
+        }
+        return array;
     }
 }
 
