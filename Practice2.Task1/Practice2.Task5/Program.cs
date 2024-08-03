8ef0b70bb82026d8414a6c4c852115125613a645
iDobryDen <59179395+iDobryDen@users.noreply.github.com>
2024-08-03 09:37:25 +0400

    Practice2.Task5 done

--- Practice2.Task1/Practice2.Task5/Program.cs
+++ Practice2.Task1/Practice2.Task5/Program.cs
@@ -1,10 +1,29 @@
 ﻿namespace Practice2.Task5;
 
+//Напишите программу для сравнения двух целых чисел. Ввод осуществляйте через консоль.
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("введите первое число");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("введите второе число");
+        int b = int.Parse(Console.ReadLine());
+
+        if (a > b)
+        {
+            Console.WriteLine("первое число больше второго");
+        }
+        else if (a == b)
+        {
+            Console.WriteLine("числа равны");
+        }
+        else
+        {
+            Console.WriteLine("первое число меньше второго");
+        }
     }
 }
 
