6529923a0562af6d4883013d2193480097683c5a
iDobryDen <59179395+iDobryDen@users.noreply.github.com>
2024-08-03 13:55:04 +0400

    Practice3.Task1 done

--- Practice3.Task1/Program.cs
+++ Practice3.Task1/Program.cs
@@ -1,10 +1,29 @@
 ﻿namespace Practice3.Task1;
 
-class Program
+//Создай класс "Студент" с полями "Имя" и "Возраст". Напиши метод, который будет выводить информацию о студенте в консоль.
+
+public class Program
 {
-    static void Main(string[] args)
+
+    public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Student student = new Student();
+        student.name = "Den";
+        student.age = 36;
+
+        student.PrintInfo();
     }
+}
+
+public class Student
+{
+    public string name;
+    public int age;
+
+    public void PrintInfo()
+    {
+        Console.WriteLine($"Имя: {name}, Возраст: {age}");
+    }
+
 }
 
