977bce76282a212639a622adf3f60a271c1ce757
iDobryDen <59179395+iDobryDen@users.noreply.github.com>
2024-08-04 12:11:25 +0400

    Practice3.Task3 continue

--- Practice3.Task1/Practice3.Task3/Program.cs
+++ Practice3.Task1/Practice3.Task3/Program.cs
@@ -1,10 +1,46 @@
-﻿namespace Practice3.Task3;
+﻿using System.Xml.Linq;
+
+namespace Practice3.Task3;
+
+//Создай класс "Калькулятор" с методами для выполнения основных арифметических операци
+//й(сложение, вычитание, умножение, деление). Пусть эти методы принимают два числа и возвращают результат операции.
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Calculator calc = new Calculator();
+
+
+        Console.WriteLine("Введите первое значение");
+        int a = int.Parse(Console.ReadLine());
     }
 }
 
+public class Calculator
+{
+
+    public void Sum(int a, int b)
+    {
+        float result = a / b;
+        Console.WriteLine(result);
+    }
+
+    public void Subtraction(int a, int b)
+    {
+        float result = a / b;
+        Console.WriteLine(result);
+    }
+
+    public void Multiplication(int a, int b)
+    {
+        float result = a / b;
+        Console.WriteLine(result);
+    }
+
+    public void Division(int a, int b)
+    {
+        float result = a / b;
+        Console.WriteLine(result);
+    }
+}
