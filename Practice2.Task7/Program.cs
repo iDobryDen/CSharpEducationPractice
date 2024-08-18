307500ecdb6bcb3ad3e6e7d45785352f8f6272a2
iDobryDen <59179395+iDobryDen@users.noreply.github.com>
2024-08-18 21:55:18 +0400

    Create Practice2.Task7

---
+++ Practice2.Task7/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Practice2.Task7;
+
+//Напишите программу, которая переводит строку в разные регистры
+    //a. в верхний регистр: f -> F
+    //b. в нижний регистр: F -> f
+    //c. делает заглавную букву в слове: привет -> Привет
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
